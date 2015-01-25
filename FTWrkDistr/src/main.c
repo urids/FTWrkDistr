70fc5362b82a7a5398bc0e05f80dbd6b71f57f13
urids <urielcabello@gmail.com>
2015-01-24 18:45:42 -0600

    composition 20150124

--- FTWrkDistr/src/main.c
+++ FTWrkDistr/src/main.c
@@ -3,15 +3,16 @@
 
 int main(int argc,char** argv){
 	int numtasks, taskid, len;
-		char hostname[MPI_MAX_PROCESSOR_NAME];
-	MPI_Init(&argc, &argv);
-		MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
-		MPI_Comm_rank(MPI_COMM_WORLD, &taskid);
-		MPI_Get_processor_name(hostname, &len);
-		printf("Hello from task %d on %s!\n", taskid, hostname);
-		MPI_Finalize();
+	char hostname[MPI_MAX_PROCESSOR_NAME];
 
-		return 0;
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+	MPI_Comm_rank(MPI_COMM_WORLD, &taskid);
+	MPI_Get_processor_name(hostname, &len);
+	printf("Hello from task %d on %s!\n", taskid, hostname);
+	MPI_Finalize();
+
+	return 0;
 
 
 }
